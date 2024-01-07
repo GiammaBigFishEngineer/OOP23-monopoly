4d7df4cb08c86266ae1d4e51a110ebbf9bfa40f0
Gianmaria Di Fronzo <gianmaria.difronzo@studio.unibo.it>
2024-01-07 12:04:10 +0100

    update MenuControllerInterface

--- src/main/java/app/game/API/MenuController.java
+++ src/main/java/app/game/API/MenuController.java
@@ -2,10 +2,8 @@ package app.game.API;
 
 import java.util.*;
 
-import app.player.API.Player;
-
 public interface MenuController {
     Boolean startGame();
     void quitGame();
-    void insertPlayers(List<Player> stringPlayer);
+    void insertPlayers(List<String> stringPlayer);
 }
