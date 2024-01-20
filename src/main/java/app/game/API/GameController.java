340a0469091533339cc5a8a690eeef895a906c31
Riccardo Polazzi <riccardo.polazzi@studio.unibo.it>
2024-01-20 11:25:25 +0100

    initialization Polazzi branch

--- src/main/java/app/game/API/GameController.java
+++ src/main/java/app/game/API/GameController.java
@@ -4,12 +4,22 @@ import app.card.API.Card;
 import app.player.API.Player;
 
 public interface GameController {
+    /**
+     * @return void
+     */
     void rollDice();
+
     void buyPropriety(Player currentPlayer);
+
     void buildHouse(Player currentPlayer);
+
     void sellPropriety(Player currentPlayer);
+
     void endTurn();
+
     void payFees(Player currentPlayer, Card box);
+
     void checkBox(Player currentPlayer, Card box);
+
     Card currentBox(Player currentPlayer);
 }
