106ab065587df56649647a506fa21affe5da4dbe
Riccardo Polazzi <riccardo.polazzi@studio.unibo.it>
2024-02-14 16:48:14 +0100

    Changed BtnCodeEnum

--- src/main/java/app/game/view/BtnCodeEnum.java
+++ src/main/java/app/game/view/BtnCodeEnum.java
@@ -2,16 +2,10 @@ package app.game.view;
 
 public enum BtnCodeEnum {
 
-    rollDice(0),
-    buyPropriety(1),
-    sellPropriety(2),
-    buyHouse(3),
-    endTurn(4);
-
-    int code;
-
-    BtnCodeEnum(int code) {
-        this.code = code;
-    }
+    rollDice(),
+    buyPropriety(),
+    sellPropriety(),
+    buyHouse(),
+    endTurn();
 
 }
