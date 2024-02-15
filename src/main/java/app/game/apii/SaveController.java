18f5aef2f9864e1228d7785d9c279699c0451e6e
Giovanni Muccioli <giovanni.muccioli@studio.unibo.it>
2024-02-15 12:53:37 +0100

    Separate option for saving and viewing data

---
+++ src/main/java/app/game/apii/SaveController.java
@@ -0,0 +1,35 @@
+package app.game.apii;
+
+import java.util.List;
+
+import app.player.apii.Player;
+
+/**
+ * An interface that defines operations that can be used for saving data.
+ */
+public interface SaveController {
+
+    /**
+     * Saves the current state of the game players to a file.
+     * 
+     * @param gamePlayerList the list of player that needs to be saved
+     */
+    void saveGame(List<Player> gamePlayerList);
+
+    /**
+     * Determines whether the game should be saved.
+     * The game should be saved if it is the first save or if there are changes in player positions or balances.
+     * It is used to know when to save game data and, consequently, to create a summary of all game sessions.
+     * 
+     * @param gamePlayerList the list of players that must be saved if there are changes
+     * @return {@code true} if the game should be saved, {@code false} otherwise.
+     */
+    boolean shouldSaveGame(List<Player> gamePlayerList);
+
+    /**
+     * Displays the data of the saved games.
+     *
+     * @return a list of strings representing the data of the saved games
+     */
+    List<String> viewSavedGames();
+}
