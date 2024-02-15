8db9be3890f0cd02394294b381cdf6b569a36bde
Giovanni Muccioli <giovanni.muccioli@studio.unibo.it>
2024-02-15 18:45:58 +0100

    Update javadoc

--- src/main/java/app/game/Hello.java
+++ src/main/java/app/game/Hello.java
@@ -3,8 +3,8 @@ package app.game;
 /**
  * The main class for launching the game application.
  * This class contains the main method responsible for starting the game
- * by creating an istance of {@link GameView} class 
- * after an istance of {@link MenuView} class.
+ * by creating an istance of {@link game.view.GameView} class 
+ * after an istance of {@link app.game.view.MenuView} class.
  * 
  * To start the application, simply run the {@code main} method of this class.
  */
